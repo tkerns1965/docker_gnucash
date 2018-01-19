fb72f6122a98f0a7ab4da151d1e37eb2edafb9f0
tkerns1965 <tony.kerns.65@gmail.com>
2018-01-19 14:23:48 -0500

    Update gnucash.sh

--- ubuntu/src/gnucash/gnucash.sh
+++ ubuntu/src/gnucash/gnucash.sh
@@ -6,7 +6,10 @@ echo "Install GnuCash"
 apt update
 apt install -y cmake libdbi-dev texinfo
 apt clean -y
-wget -qO- https://superb-dca2.dl.sourceforge.net/projects/gnucash/gnucash%20%28stable%29/2.6.19/gnucash-2.6.19.tar.gz | tar xz --strip 1 -C /headless/gnucash_source
+mkdir -p /headless/gnucash_source
+# https://sourceforge.net/projects/gnucash/files/gnucash%20%28stable%29/2.6.19/gnucash-2.6.19.tar.gz/download
+# https://superb-dca2.dl.sourceforge.net/projects/gnucash/gnucash%20%28stable%29/2.6.19/gnucash-2.6.19.tar.gz
+wget -qO- https://sourceforge.net/projects/gnucash/files/gnucash%20%28stable%29/2.6.19/gnucash-2.6.19.tar.gz | tar xz --strip 1 -C /headless/gnucash_source
 # mkdir build-cmake
 # cd build-cmake
 # cmake -D CMAKE_INSTALL_PREFIX=<target>/gnucash-cmake -D WITH_AQBANKING=OFF -D WITH_OFX=OFF ../gnucash
